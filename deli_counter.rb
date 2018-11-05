1e1e1d116afdd442219d5d84e811d4a4b7038d3a
sensible-proc-7557 <vchan313@gmail.com>
2018-11-05 14:25:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,14 +4,15 @@ def line (array1)
   counter = 1
   
   array1.each do |element|
-    line.push("#{counter}.#{element} ")
+    line.push("#{counter}. #{element} ")
     counter += 1
   end
   
   if array1.length == 0  
     puts "The line is currently empty."
   else 
-    puts("The line is currently: " + line)
-    
+    puts("The line is currently:  #{line}")
   end
 end
+
+line(katz_deli)
